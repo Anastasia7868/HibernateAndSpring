df9901ef1d7c6658bc4aadf26dbe2d411d643624
Anastasia7868 <nastya7868@gmail.com>
2022-09-08 22:59:29 +0300

    create customer entity

---
+++ src/main/java/ru/aston/appolinarova/hibernate/models/Customer.java
@@ -0,0 +1,74 @@
+package ru.aston.appolinarova.hibernate.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Customer")
+public class Customer {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "customer_name")
+    private String customerName;
+
+    @Column(name = "account")
+    private String account;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Project> projects;
+
+    public Customer() {
+    }
+
+    public Customer(String customerName, String account, String email) {
+        this.customerName = customerName;
+        this.account = account;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+}
